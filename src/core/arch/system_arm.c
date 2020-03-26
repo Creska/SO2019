600cfd904422d677357865971c5a124a305793dc
maomettino <56378319+maomettino@users.noreply.github.com>
2020-03-26 03:25:10 +0100

    Update system_arm.c

--- src/core/arch/system_arm.c
+++ src/core/arch/system_arm.c
@@ -1,32 +1,35 @@
 #include "core/system.h"
-#include "utils/utils.h"
-#define SP_INDEX  28
-//the following three lines are copied from p1.5test_bikaya_v0.c
-#define RAMBASE    *((unsigned int *)BUS_REG_RAM_BASE)//BUS_REG_RAM_BASE and BUS_REG_RAM_SIZE are defined in arch.h
-#define RAMSIZE    *((unsigned int *)BUS_REG_RAM_SIZE)//as 0x10000000 and 0x10000004 respectively
-#define RAMTOP     (RAMBASE + RAMSIZE)
+
 void reset_state(state_t* s) {
-    s->pc_epc = 0;
-    s->hi = 0;
-    s->entry_hi = 0;
-    s->cause = 0;
-    s->status = 0;
-    s->lo = 0;
-    for (int i = 0; i < STATE_GPR_LEN; ++i) {
-        s->gpr[i] = 0;
-    }
+    s->a1 = 0;
+    s->a2 = 0;
+    s->a3 = 0;
+    s->a4 = 0;
+    s->CP15_Cause = 0;
+    s->CP15_Control = 0;
+    s->cpsr = 0;
+    s->TOD_Hi = 0;
+    s->TOD_Low = 0;
+    s->fp = 0;
+    s->lr = 0;
+    s->pc = 0;
+    s->sp = 0;
+    s->sl = 0;
+    s->ip = 0;
+    s->v1 = 0;
+    s->v2 = 0;
+    s->v3 = 0;
+    s->v4 = 0;
+    s->v5 = 0;
+    s->v6 = 0;
+    s->CP15_EntryHi = 0;
 }
 //state_t della new_area
 void set_pc(state_t * s, void (*ptr)()) {//ptr puntatore all'handler assumendolo con la signature placeholder void handler()
-s->pc_epc = (unsigned int)ptr;
+s->pc = (unsigned int)ptr;
 }
 
 void set_sp(state_t *s) {
-    s->gpr[SP_INDEX] = RAMTOP;
+    s->sp = RAM_TOP;//RAM_TOP is defined in arch.h with the value needed
 }
-
-
-//    s->status = set_bit_at(s->status, on, STATUS_KUc_BIT);
 }
-
-
