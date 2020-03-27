48a8fd5eb9bbb024fe9cd4e397152224080102c3
maomettino <56378319+maomettino@users.noreply.github.com>
2020-03-27 02:56:30 +0100

    Update system_mps.c

--- src/core/arch/system_mps.c
+++ src/core/arch/system_mps.c
@@ -1,6 +1,7 @@
 #include "core/system.h"
 #include "utils/utils.h"
 #define SP_INDEX  28
+#define T_INDEX 26 //t9 register index assuming it is the 26th gpr which is uncertain
 //the following three lines are copied from p1.5test_bikaya_v0.c
 #define RAMBASE    *((unsigned int *)BUS_REG_RAM_BASE)//BUS_REG_RAM_BASE and BUS_REG_RAM_SIZE are defined in arch.h
 #define RAMSIZE    *((unsigned int *)BUS_REG_RAM_SIZE)//as 0x10000000 and 0x10000004 respectively
@@ -17,8 +18,9 @@ void reset_state(state_t* s) {
     }
 }
 //state_t della new_area
-void set_pc(state_t * s, void (*ptr)()) {//ptr puntatore all'handler assumendolo con la signature placeholder void handler()
+void set_pc(state_t * s, void (*ptr)()) {//ptr puntatore all'handler assumendolo con la signature void handler()
 s->pc_epc = (unsigned int)ptr;
+s->gpr[T_INDEX] = (unsigned int)ptr;
 }
 
 void set_sp(state_t *s) {
