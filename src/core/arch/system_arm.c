cfdf42de0cb19917b8f58bde3cdaf7f7c391aa91
Creska <luca.paparo@studio.unibo.it>
2020-03-29 19:40:26 +0200

    added set kernel mode for arm and utils function

--- src/core/arch/system_arm.c
+++ src/core/arch/system_arm.c
@@ -1,6 +1,7 @@
 #include "core/system.h"
 
-void reset_state(state_t* s) {
+void reset_state(state_t *s)
+{
     s->a1 = 0;
     s->a2 = 0;
     s->a3 = 0;
@@ -26,19 +27,38 @@ void reset_state(state_t* s) {
 }
 
 //state_t della new_area
-void set_pc(state_t * s, void (*ptr)()) {//ptr puntatore all'handler assumendolo con la signature placeholder void handler()
-s->pc = (unsigned int)ptr;
+void set_pc(state_t *s, void (*ptr)())
+{ //ptr puntatore all'handler assumendolo con la signature placeholder void handler()
+    s->pc = (unsigned int)ptr;
 }
 
-void set_sp(state_t *s, unsigned int sp_val) {
-    s->sp = sp_val;         //RAM_TOP is defined in arch.h with the value needed
-
+void set_sp(state_t *s, unsigned int sp_val)
+{
+    s->sp = sp_val; //RAM_TOP is defined in arch.h with the value needed
 }
-void set_virtual_mem(state_t* s, unsigned int on){
-    if(on){
+void set_virtual_mem(state_t *s, unsigned int on)
+{
+    if (on)
+    {
         s->CP15_Control = CP15_ENABLE_VM(s->CP15_Control);
     }
-    else{
+    else
+    {
         s->CP15_Control = CP15_DISABLE_VM(s->CP15_Control);
+    }
+}
+
+void set_kernel_mode(state_t *s, unsigned int on)
+{
+    int mask = 0x0000001F;
+    if (on)
+    {
+        s->cpsr = set_bits(s->cpsr, mask, 0x0000001F);
+                                    
+    }
+    else
+    {
+        s->cpsr = set_bits(s->cpsr, mask, 0x00000010);
+                                    
     }
 }
