8b78317faf1d111558159c3e1eb440ae36c15d59
Creska <luca.paparo@studio.unibo.it>
2020-03-29 20:15:10 +0200

    changed function name due to merge error

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -1,6 +1,6 @@
 #ifndef BIKAYA_PHASE0_UTILS_H
 #define BIKAYA_PHASE0_UTILS_H
 
-unsigned int set_bit_at(unsigned int original, unsigned int bit, unsigned int pos);
+unsigned int set_bits(unsigned int original, unsigned int bit, unsigned int pos);
 
 #endif //BIKAYA_PHASE0_UTILS_H
